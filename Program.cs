a9f50691dbbdab415741ca4b4250ccb1753f48ee
wellahealthschool github <dlaweyenghe@gmil.com>
2022-04-23 01:12:00 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.Write("Enter firstNumber: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter secondNumber: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int sum = firstNumber + secondNumber;
+            if(sum < 100) {
+                Console.WriteLine("true");
+            } else {
+                Console.WriteLine("false");
+            }
+
+
+            Console.ReadLine();
